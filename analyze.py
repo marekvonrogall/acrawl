7dd688084acb2b01f558aaa7b25a66b022b9789a
Marek von Rogall <marekvonrogall@gmx.ch>
2025-08-29 14:32:32 +0200

    Create SSN plot with highlighted M/X-Flares using matplotlib

---
+++ analyze.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def analyze_data(normalized_data):
+    data_map = {d['name']: d['data_frame'] for d in normalized_data}
+    start_date = pd.Timestamp.today() - pd.Timedelta(days=8)
+
+    df_solar = data_map['daily_solar_cycle']
+    df_solar['datetime'] = pd.to_datetime(df_solar['datetime']).dt.tz_localize(None)
+    df_solar = df_solar[df_solar['datetime'] >= start_date]
+    df_solar.set_index('datetime', inplace=True)
+
+    flare_frames = []
+    for flare_name in ['xray_flares_week_primary', 'xray_flares_week_secondary']: flare_frames.append(data_map[flare_name])
+    df_flares = pd.concat(flare_frames)
+    df_flares['datetime'] = pd.to_datetime(df_flares['datetime']).dt.tz_localize(None)
+    df_flares = df_flares[(df_flares['datetime'] >= start_date) & (df_flares['max_class'].str.startswith(('M', 'X')))]
+    flare_dates = df_flares['datetime'].dt.normalize().drop_duplicates()
+    flare_dates_filtered = flare_dates[flare_dates.isin(df_solar.index)]
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(df_solar.index, df_solar['swpc_ssn'], label='SWPC SSN')
+    plt.scatter(flare_dates_filtered, df_solar.loc[flare_dates_filtered, 'swpc_ssn'], color='purple', label='M/X Flares', zorder=5)
+    plt.title('Sunspot Number with highlighted M/X-Flares (past week)')
+    plt.xlabel('Date')
+    plt.ylabel('Sunspot Number')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from parse import parse_data
 from normalize import normalize_dates
 from database import store_data
 from download import fetch_data
+from analyze import analyze_data
 
 if __name__ == '__main__':
     fetch_data(get_daily_cme_movie_frames=False)
     parsed_data, _ = parse_data()
     normalized_data = normalize_dates(parsed_data)
     store_data(normalized_data)
+    analyze_data(normalized_data)
